592d3b4da7372a905405658b1fd192ed8b61df71
p.galmiche <p.galmiche@unistra.fr>
2024-02-21 08:46:14 +0100

    fix: update ports to allow run together with julia container + update Dockerfile for ownership of bind volume

--- demos/node_js/gulpfile.js
+++ demos/node_js/gulpfile.js
@@ -21,7 +21,7 @@ const connect = require('gulp-connect')
 const autoprefixer = require('gulp-autoprefixer')
 
 const root = yargs.argv.root || '.'
-const port = yargs.argv.port || 8000
+const port = yargs.argv.port || 7346
 const host = yargs.argv.host || '0.0.0.0'
 
 const banner = `/*!
